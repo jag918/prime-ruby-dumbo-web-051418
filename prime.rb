d68694dca8408be07c0c146f8bb4563dd54db68c
jee <jag918@nyu.edu>
2018-05-05 04:52:08 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(number)
+  number_count=2..number-1
+  for i in number_count 
+    puts i 
+    if number % i==0 || number % -i==0 
+      puts true
+      true 
+    else
+      puts false
+      false
+    end
+  end
+end
+
+prime?(-11)
